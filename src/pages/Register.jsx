3dc56875b9aa9bb8198120c83e1d6390e37a4565
vaibhav-chittora <CHITTORAVAIBHAV70@GMAIL.COM>
2025-03-20 19:49:49 +0530

    register -bugfix

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const Register = () => {
     const [name, setName] = useState('');
